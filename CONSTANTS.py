028160d54360c8b450dcab000f09903f26ed38df
mjcarr <mjcarr@udel.edu>
2019-11-15 10:21:49 -0500

    Added first popcorn image

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -5,8 +5,8 @@ BACKGROUND_COLOR = arcade.color.BLACK
 GAME_TITLE = "Popcorn Frog"
 GAME_SPEED = 1/60
 FROG = arcade.load_texture("images/cutepixelfrog_16x16.gif", scale = 3)
-POPCORN = arcade.load_texture("images/popcorn.png")
-CANDY = arcade.load_texture("images/blob.png", scale = .125)
+POPCORN = arcade.load_texture("images/PopcornDraw.png", scale = .0625)
+CANDY = arcade.load_texture("images/blob.png", scale = .1)
 MOVEMENT_SPEED = 5
 FALL_SPEED = 1
 MAX_LENGTH = 100
